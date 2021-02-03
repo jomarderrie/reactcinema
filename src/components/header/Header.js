77f8bf39d73440e4ee544ad5984d338d54611c39
nathan ganesh <489290@student.saxion.nl>
2021-02-03 20:03:41 +0100

    final push

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { useHistory, useLocation, useRouteMatch } from 'react-router-dom';
 
 import './Header.scss';
 import logo from '../../assets/cinema-logo.svg';
 import {
   getMovies,
   setMovieType,
-  setResponsePageNumber
+  setResponsePageNumber,
+  searchQuery,
+  searchResult,
+  clearMovieDetails
 } from '../../redux/actions/movies';
 
 const HEADER_LIST = [
@@ -43,22 +47,59 @@ const Header = (props) => {
     setMovieType,
     page,
     totalPages,
-    setResponsePageNumber
+    setResponsePageNumber,
+    searchQuery,
+    searchResult,
+    clearMovieDetails
   } = props;
   let [navClass, setNavClass] = useState(false);
   let [menuClass, setMenuClass] = useState(false);
   const [type, setType] = useState('now_playing');
+  const [search, setSearch] = useState('');
+  const [disableSearch, setDisableSearch] = useState(false);
+  const [hideHeader, setHideHeader] = useState(false);
+
+  const history = useHistory();
+  const location = useLocation();
+  const detailsRoute = useRouteMatch('/:id/:name/details');
 
   useEffect(() => {
     getMovies(type, page);
     setResponsePageNumber(page, totalPages);
+    if (detailsRoute || location.pathname === '/') {
+      setHideHeader(true);
+    }
+
+    if (location.pathname !== '/' && location.key) {
+      setDisableSearch(true);
+    }
 
     // eslint-disable-next-line
-  }, [type]);
+  }, [type, disableSearch, location]);
 
   const setMovieTypeUrl = (type) => {
-    setType(type);
-    setMovieType(type);
+    setDisableSearch(false);
+    if (location.pathname !== '/') {
+      clearMovieDetails();
+      history.push('/');
+      setType(type);
+      setMovieType(type);
+    } else {
+      setType(type);
+      setMovieType(type);
+    }
+  };
+
+  const onSearchChange = (e) => {
+    setSearch(e.target.value);
+    searchQuery(e.target.value);
+    searchResult(e.target.value);
+  };
+
+  const navigateToMainPage = () => {
+    setDisableSearch(false);
+    clearMovieDetails();
+    history.push('/');
   };
 
   const toggleMenu = () => {
@@ -75,53 +116,59 @@ const Header = (props) => {
 
   return (
     <>
-      <div className="header-nav-wrapper">
-        <div className="header-bar"></div>
-        <div className="header-navbar">
-          <div className="header-image">
-            <img src={logo} alt="" />
+      {hideHeader && (
+        <div className="header-nav-wrapper">
+          <div className="header-bar"></div>
+          <div className="header-navbar">
+            <div className="header-image" onClick={() => navigateToMainPage()}>
+              <img src={logo} alt="" />
+            </div>
+            <div
+              className={`${
+                menuClass
+                  ? 'header-menu-toggle is-active'
+                  : 'header-menu-toggle'
+              }`}
+              id="header-mobile-menu"
+              onClick={() => toggleMenu()}
+            >
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+            </div>
+            <ul
+              className={`${
+                navClass ? 'header-nav header-mobile-nav' : 'header-nav'
+              }`}
+            >
+              {HEADER_LIST.map((data) => (
+                <li
+                  key={data.id}
+                  className={
+                    data.type === type
+                      ? 'header-nav-item active-item'
+                      : 'header-nav-item'
+                  }
+                  onClick={() => setMovieTypeUrl(data.type)}
+                >
+                  <span className="header-list-name">
+                    <i className={data.iconClass}></i>
+                  </span>
+                  &nbsp;
+                  <span className="header-list-name">{data.name}</span>
+                </li>
+              ))}
+              <input
+                className={`search-input ${disableSearch ? 'disabled' : ''}`}
+                type="text"
+                placeholder="Search for a movie"
+                value={search}
+                onChange={onSearchChange}
+              />
+            </ul>
           </div>
-          <div
-            className={`${
-              menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'
-            }`}
-            id="header-mobile-menu"
-            onClick={() => toggleMenu()}
-          >
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </div>
-          <ul
-            className={`${
-              navClass ? 'header-nav header-mobile-nav' : 'header-nav'
-            }`}
-          >
-            {HEADER_LIST.map((data) => (
-              <li
-                key={data.id}
-                className={
-                  data.type === type
-                    ? 'header-nav-item active-item'
-                    : 'header-nav-item'
-                }
-                onClick={() => setMovieTypeUrl(data.type)}
-              >
-                <span className="header-list-name">
-                  <i className={data.iconClass}></i>
-                </span>
-                &nbsp;
-                <span className="header-list-name">{data.name}</span>
-              </li>
-            ))}
-            <input
-              className="search-input"
-              type="text"
-              placeholder="Search for a movie"
-            />
-          </ul>
         </div>
-      </div>
+      )}
     </>
   );
 };
@@ -129,14 +176,15 @@ const Header = (props) => {
 Header.propTypes = {
   getMovies: PropTypes.func,
   setMovieType: PropTypes.func,
+  searchQuery: PropTypes.func,
+  searchResult: PropTypes.func,
+  clearMovieDetails: PropTypes.func,
   setResponsePageNumber: PropTypes.func,
-  // list: PropTypes.array,
   page: PropTypes.number,
   totalPages: PropTypes.number
 };
 
 const mapStateToProps = (state) => ({
-  // list: state.movies.list,
   page: state.movies.page,
   totalPages: state.movies.totalPages
 });
@@ -144,5 +192,8 @@ const mapStateToProps = (state) => ({
 export default connect(mapStateToProps, {
   getMovies,
   setMovieType,
-  setResponsePageNumber
+  setResponsePageNumber,
+  searchQuery,
+  searchResult,
+  clearMovieDetails
 })(Header);
--- src/redux/actions/movies.js
+++ src/redux/actions/movies.js
@@ -3,14 +3,31 @@ import {
   SET_ERROR,
   RESPONSE_PAGE,
   LOAD_MORE_RESULTS,
-  MOVIE_TYPE
+  MOVIE_TYPE,
+  SEARCH_QUERY,
+  SEARCH_RESULT,
+  MOVIE_DETAILS,
+  CLEAR_MOVIE_DETAILS
 } from '../types';
-import { MOVIE_API_URL } from '../../services/movies.services.js';
+import {
+  MOVIE_API_URL,
+  SEARCH_API_URL,
+  MOVIE_DETAILS_URL,
+  MOVIE_CREDITS_URL,
+  MOVIE_IMAGES_URL,
+  MOVIE_VIDEOS_URL,
+  MOVIE_REVIEWS_URL
+} from '../../services/movies.service';
 
 export const getMovies = (type, pageNumber) => async (dispatch) => {
   try {
     const response = await getMoviesRequest(type, pageNumber);
-    const { results, payload } = response;
+    let { results, payload } = response;
+    results = results.filter((item) => {
+      return !item.original_title.includes('%');
+    });
+    console.log();
+
     dispatchMethod(MOVIE_LIST, results, dispatch);
     dispatchMethod(RESPONSE_PAGE, payload, dispatch);
   } catch (error) {
@@ -36,6 +53,46 @@ export const loadMoreMovies = (type, pageNumber) => async (dispatch) => {
   }
 };
 
+export const searchResult = (query) => async (dispatch) => {
+  try {
+    if (query) {
+      const movies = await SEARCH_API_URL(query);
+      const { results } = movies.data;
+      dispatchMethod(SEARCH_RESULT, results, dispatch);
+    } else {
+      dispatchMethod(SEARCH_RESULT, [], dispatch);
+    }
+  } catch (error) {
+    if (error.response) {
+      dispatchMethod(SET_ERROR, error.response.data.message, dispatch);
+    }
+  }
+};
+
+export const movieDetails = (id) => async (dispatch) => {
+  try {
+    console.log(id);
+    const details = await MOVIE_DETAILS_URL(id);
+    const credits = await MOVIE_CREDITS_URL(id);
+    const images = await MOVIE_IMAGES_URL(id);
+    const videos = await MOVIE_VIDEOS_URL(id);
+    const reviews = await MOVIE_REVIEWS_URL(id);
+
+    const resp = await Promise.all([details, credits, images, videos, reviews])
+      .then((values) => Promise.all(values.map((value) => value.data)))
+      .then((response) => response);
+    dispatchMethod(MOVIE_DETAILS, resp, dispatch);
+  } catch (error) {
+    if (error.response) {
+      dispatchMethod(SET_ERROR, error.response.data.message, dispatch);
+    }
+  }
+};
+
+export const clearMovieDetails = () => async (dispatch) => {
+  dispatchMethod(CLEAR_MOVIE_DETAILS, [], dispatch);
+};
+
 export const setResponsePageNumber = (page, totalPages) => async (dispatch) => {
   const payload = { page, totalPages };
   dispatchMethod(RESPONSE_PAGE, payload, dispatch);
@@ -43,6 +100,10 @@ export const setResponsePageNumber = (page, totalPages) => async (dispatch) => {
 
 export const setMovieType = (type) => async (dispatch) => {
   dispatchMethod(MOVIE_TYPE, type, dispatch);
+};
+
+export const searchQuery = (query) => async (dispatch) => {
+  dispatchMethod(SEARCH_QUERY, query, dispatch);
 };
 
 const dispatchMethod = (type, payload, dispatch) => {
