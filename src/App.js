260e786e947a978c46fd5c7353f45b49cbb061b5
nathan ganesh <489290@student.saxion.nl>
2021-02-03 14:00:11 +0100

    slide show

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import './App.scss';
 import store from './redux/store';
 import Header from './components/header/Header';
+import Main from './components/main/Main';
+
 const App = () => {
   return (
     <Provider store={store}>
-    <Header/>
+      <Header />
       <div className="app">
-
+        <Main />
       </div>
     </Provider>
   );
